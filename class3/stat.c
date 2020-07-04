a5a02e6c2963afb2ac155b5ba77d2b48b9e71e82
takuan517 <ishitaku-2000517@keio.jp>
2020-07-04 21:06:08 +0900

    add stat

---
+++ class3/stat.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("error\n");
+        return 1;
+    }
+    struct stat buf;
+    if (stat(argv[1], &buf) < 0)
+    {
+        perror("stat");
+        return 0;
+    }
+    printf("ファイルに関する情報\n");
+    printf("    st_mode : %#o\n", buf.st_mode);
+    printf("    st_uid  : %ld\n", (long)buf.st_uid);
+    printf("    st_gid  : %ld\n", (long)buf.st_gid);
+    printf("    st_size : %ld\n", (long)buf.st_size);
+    printf("    st_atimespec(秒) : %ld\n", (long)buf.st_atimespec.tv_sec);
+    printf("    st_mtimespec(秒) : %ld\n", (long)buf.st_mtimespec.tv_sec);
+    printf("    st_ctimespec(秒) : %ld\n", (long)buf.st_ctimespec.tv_sec);
+}
